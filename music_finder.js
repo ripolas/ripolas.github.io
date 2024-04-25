fffbe239398ec147dc56cf4fc1437f0f3d338f06
Rapolas <rapolaspreiksaitis@gmail.com>
2024-04-25 07:51:01 +0300

    video_test

--- music_finder.js
+++ music_finder.js
@@ -1,10 +1,15 @@
 let songs;
+var video;
+var show_video = true;
 function preload(){
-  songs = loadJSON("/data_final.json");
+  songs = loadJSON("/data.json");
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
   console.log(songs[0]["genres"]);
+  let video_id = songs[0]["video_id"];
+  video = createDiv('<iframe width="420" height="345" src="http://www.youtube.com/embed/'+video_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>')
+  video.position(0,0);
 }
 
 
