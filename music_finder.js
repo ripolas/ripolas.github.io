7f93796063d2d5077f1ae0be57affee5f83ae2cd
Rapolas <rapolaspreiksaitis@gmail.com>
2024-04-25 11:59:09 +0300

    slier & button

--- music_finder.js
+++ music_finder.js
@@ -1,26 +1,23 @@
 let songs;
 let video;
-let buttons = [];
-let button_size;
 let current_song_id = 1;
+let slider_size;
 function preload(){
   songs = loadJSON("/data.json");
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  button_size = width/9;
-  for(let i = 0;i<9;i++){
-    buttons[i] = createButton(i+1);
-    buttons[i].position(i*button_size,height-button_size);
-    buttons[i].size(button_size,button_size);
-  }
+  slider_size = width/9;
   console.log(songs[current_song_id]["genres"]);
   let video_id = songs[current_song_id]["video_id"];
-  video = createDiv('<iframe width="'+width+'" height="'+(height-button_size)+'" src="https://www.youtube.com/embed/'+video_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>')
+  video = createDiv('<iframe width="'+width+'" height="'+(height-slider_size)+'" src="https://www.youtube.com/embed/'+video_id+'?autoplay=1" frameborder="0" allowfullscreen></iframe>')
   video.position(0,0);
   
 }
-
-
 function draw() {
+  background(0);
+  fill(255);
+  for(let i = 0; i < width;i++){
+    text(i+1,i*(width-25)/8+12,height-25-8);
+  }
 }
